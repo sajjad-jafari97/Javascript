06f8a7817474b5b8da2b492fafe7b53016885a1f
sajjad <sajjadjafari081@gmail.com>
2017-10-17 12:01:41 +0200

    'Exo-js-jquery'

--- js-elements/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-function calcul(){
-    var x = document.getElementById('inp1').value;
-    var y = document.getElementById('inp2').value;
-    var z = document.getElementById('total').value= x * y;
-     if( isNaN(x) || isNaN(y)) {
-        alert("vous devriez taper que des nombres!!!!!");
-    }
-}
-calcul();

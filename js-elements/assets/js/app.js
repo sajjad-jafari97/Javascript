5815f469af00ea7ef9dd81f98f6b2fdb1269b824
sajjad <sajjadjafari081@gmail.com>
2017-10-17 10:59:50 +0200

    'calculator-form'

---
+++ js-elements/assets/js/app.js
@@ -0,0 +1,6 @@
+function calcul(){
+    var x = document.getElementById('inp1').value;
+    var y = document.getElementById('inp2').value;
+    var z = document.getElementById('total').value= x * y;
+}
+calcul();

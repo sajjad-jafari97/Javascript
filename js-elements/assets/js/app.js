77f154b00f6c4337df76458924267c216b9502ad
sajjad <sajjadjafari081@gmail.com>
2017-10-17 11:55:22 +0200

    'correction'

--- js-elements/assets/js/app.js
+++ js-elements/assets/js/app.js
@@ -2,5 +2,8 @@ function calcul(){
     var x = document.getElementById('inp1').value;
     var y = document.getElementById('inp2').value;
     var z = document.getElementById('total').value= x * y;
+     if( isNaN(x) || isNaN(y)) {
+        alert("vous devriez taper que des nombres!!!!!");
+    }
 }
 calcul();
